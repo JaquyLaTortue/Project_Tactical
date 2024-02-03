37352d40810f0a672dadf34a96002747b90dd8fe
JaquyLaTortue <93046036+JaquyLaTortue@users.noreply.github.com>
2024-02-03 18:24:44 +0100

    Test turn started

--- Assets/Scripts/Turn/Select.cs
+++ Assets/Scripts/Turn/Select.cs
@@ -1,6 +1,64 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Select : MonoBehaviour
 {
+    [SerializeField] private TurnManager _turnManager;
+    private Ray _ray;
 
+    public void SelectPlayer(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started && _turnManager.PlayerTurn)
+        {
+            Debug.Log("Selecting Player");
+            _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(_ray, out hit))
+            {
+                GameObject current = hit.transform.gameObject;
+                Debug.Log("Hit: " + current.name);
+
+                if (_turnManager.Character != current && current.CompareTag("Character"))
+                {
+                    _turnManager.SetCharacter(current);
+                }
+
+                _turnManager.InputManager.SwitchCurrentActionMap("EmptyActionMap");
+            }
+        }
+        else if (ctx.started && !_turnManager.PlayerTurn)
+        {
+            Debug.Log("Not Player's Turn");
+            _turnManager.InputManager.SwitchCurrentActionMap("EmptyActionMap");
+        }
+    }
+
+    public void SelectTarget(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started && _turnManager.PlayerTurn)
+        {
+            Debug.Log("Selecting Target");
+            _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(_ray, out hit))
+            {
+                GameObject current = hit.transform.gameObject;
+                Debug.Log($"Hit: {current.name}");
+
+                if (_turnManager.Target != current && current.CompareTag("Ennemy"))
+                {
+                    _turnManager.SetTarget(current);
+                }
+
+                _turnManager.InputManager.SwitchCurrentActionMap("EmptyActionMap");
+            }
+        }
+        else if (ctx.started && !_turnManager.PlayerTurn)
+        {
+            Debug.Log("Not Player's Turn");
+            _turnManager.InputManager.SwitchCurrentActionMap("EmptyActionMap");
+        }
+    }
 }
--- Assets/Scripts/Turn/TurnManager.cs
+++ Assets/Scripts/Turn/TurnManager.cs
@@ -1,6 +1,53 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TurnManager : MonoBehaviour
 {
 
+    public int Turnindex { get; private set; } = 0;
+
+    public bool PlayerTurn { get; private set; } = true;
+
+    public GameObject Character { get; private set; }
+
+    public GameObject Target { get; private set; }
+
+    [field: SerializeField]
+    public PlayerInput InputManager { get; private set; }
+
+    public void SetCharacter(GameObject character)
+    {
+        string oldcharacter = Character == null ? "null" : Character.name;
+        Character = character;
+        Debug.Log($"Character changement: old character : {oldcharacter} and new character : {character.name}");
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        string oldtarget = Target == null ? "null" : Target.name;
+        Target = target;
+        Debug.Log($"Target changement: old Target: {oldtarget} and new character : {target.name}");
+    }
+
+    public void EndTurn()
+    {
+        Turnindex++;
+        InputManager.SwitchCurrentActionMap("EmptyActionMap");
+        DetermineTurn();
+    }
+
+    private void DetermineTurn()
+    {
+        switch (Turnindex % 2)
+        {
+            case 0:
+                Debug.Log("Player's Turn");
+                PlayerTurn = true;
+                break;
+            case 1:
+                Debug.Log("Monster's Turn");
+                PlayerTurn = false;
+                break;
+        }
+    }
 }
