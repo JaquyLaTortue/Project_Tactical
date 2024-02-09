df4b0cb1ebfb6012160921fc255c571a9416b6f2
titouan garnier <garniertitouan4@gmail.com>
2024-02-09 11:49:03 +0100

    Blocking ui

---
+++ Assets/Scripts/UI/LoadingScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField]
+    private string _nameScene;
+
+    public void LoadScenne()
+    {
+        SceneManager.LoadScene(_nameScene);
+    }
+}
---
+++ Assets/Scripts/UI/TestB.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestB : MonoBehaviour
+{
+
+
+    public void truc()
+    {
+        Debug.Log("toucher");
+    }
+}
