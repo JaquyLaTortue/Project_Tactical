0ac40ee10b500bb6ba051e66c999cad0293e25e3
m v <morgane.voisin@e-artsup.net>
2024-02-09 14:02:29 +0100

    MonstersBehaviour

---
+++ Assets/Scripts/Entities/Monsters/MonsterBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public abstract class MonsterBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    protected MonsterMain _monsterMain;
+
+    [SerializeField]
+    protected CharacterMain _targetPlayer;
+
+    [SerializeField]
+    protected EntitiesManager _entitiesManager;
+}
