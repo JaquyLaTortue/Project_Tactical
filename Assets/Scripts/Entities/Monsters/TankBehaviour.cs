96fe657d0e609b7405df77cfcb6fb1c1bb43a942
m v <morgane.voisin@e-artsup.net>
2024-02-12 10:22:18 +0100

    Movement Enemi && Attack

--- Assets/Scripts/Entities/Monsters/TankBehaviour.cs
+++ Assets/Scripts/Entities/Monsters/TankBehaviour.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
 
-public class TankBehaviour : MonoBehaviour
+public class TankBehaviour : MonsterBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        _turnManager.OnMonsterTurn += DetectePlayer;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DetectePlayer()
     {
-        
+        int distance = 1000;
+        List<WayPoint> path = new List<WayPoint>();
+        for (int i = 0; i < _entitiesManager.allCharacters.Count; i++)
+        {
+            path = _monsterMain.MonsterCapacity._mapMain.aStar.GiveThePath(_monsterMain.Position, _entitiesManager.allCharacters[i].Position);
+            if (distance < path.Count)
+            {
+                distance = path.Count;
+                _targetPlayer = _entitiesManager.allCharacters[i];
+            }
+        }
+        if (_targetPlayer.Position.casePosition[0] <= _monsterMain.Position.casePosition[0] + _monsterMain.Range)
+        {
+            if (_targetPlayer.Position.casePosition[1] <= _monsterMain.Position.casePosition[1] + _monsterMain.Range)
+            {
+                _monsterMain.MonsterCapacity.Attack(_targetPlayer);
+            }
+        }
+        else
+        {
+            _monsterMain.MonsterCapacity.Move(_targetPlayer.Position);
+        }
     }
 }
