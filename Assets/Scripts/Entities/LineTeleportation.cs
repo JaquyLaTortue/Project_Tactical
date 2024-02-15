cff26cf15c65cc8a08d2dd6e5b1aec461f682e28
Aurelien Tavernier <aurelientav@gmail.com>
2024-02-15 22:40:09 +0100

    teleportation test

---
+++ Assets/Scripts/Entities/LineTeleportation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class LineTeleportation : MonoBehaviour
+{
+    LineRenderer _line;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+        Vector3[] positions = new Vector3[3];
+        positions[0] = new Vector3(-2.0f, -2.0f, 0.0f);
+        positions[1] = new Vector3(0.0f, 2.0f, 0.0f);
+        positions[2] = new Vector3(2.0f, -2.0f, 0.0f);
+        _line.positionCount = positions.Length;
+        _line.SetPositions(positions);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
